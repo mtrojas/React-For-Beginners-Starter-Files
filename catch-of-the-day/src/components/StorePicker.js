a8664c102834e7636de7a3abdcebfed8f3633a4d
mtrojas <tererojas.i@gmail.com>
2018-12-01 17:30:50 -0300

    Create new Component AddFishForm and modify App.js and Inventory.js in order to make available the change of state.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,27 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = event => {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // 2. Get the text from that input
+    const storeName = this.myInput.value.value;
+    // 3. Change the pa ge to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`);
+  }
   render() {
     return (
-      <form action="" className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please enter a store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={ getFunName() } />
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store -> </button>
       </form>
     );
