64deff42912b1163ef84c3cd47c83689b58406ae
mtrojas <tererojas.i@gmail.com>
2019-01-08 19:03:43 -0300

    Animate React Components, Component Validation with PropTypes and Authentication

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import firebase from 'firebase';
 import AddFishForm from './AddFishForm';
 import EditFishForm from './EditFishForm';
+import Login from './Login';
+import { firebaseApp } from '../base';
 
 class Inventory extends React.Component {
+  static propTypes = {
+    fishes: PropTypes.object,
+    updateFish: PropTypes.func, 
+    deleteFish: PropTypes.func,
+    loadSampleFishes: PropTypes.func,
+    addFish: PropTypes.func
+  };
+
+  authHandler = async (authData) => {
+    // 1. Look up the current store in the firebase database
+    // 2. Claim it if there is no owner
+    // 3. Set the state of the inventory component to reflect the current user
+
+    console.log(authData);
+  }
+  authenticate = provider => {
+    const authProvider = new firebase.auth[`${provider}AuthProvider`]();
+    firebaseApp
+      .auth()
+      .signInWithPopup(authProvider)
+      .then(this.authHandler);
+  }
+
   render() {
+    return <Login authenticate={this.authenticate}/>
     return (
       <div className="inventory">
         <h2>Inventory</h2>
